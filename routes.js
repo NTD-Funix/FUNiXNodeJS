0d8c83e5ecb62546d4ddcc888855706007f22870
NTD-Funix <ntd.funix@gmail.com>
2022-07-26 00:36:58 +0700

    LAB01.9_Restarting the Debugger Automatically After Editing our App

--- routes.js
+++ routes.js
@@ -27,7 +27,6 @@ const requestHandler = (req, res) => {
             });     
         });
     };
-    
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
