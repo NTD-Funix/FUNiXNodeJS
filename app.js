5734503b76c6273b28253f357c910a90c5758b8a
NTD-Funix <ntd.funix@gmail.com>
2022-07-26 01:37:47 +0700

    LAB02.3_How Middleware Works

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const app = express();
 
 app.use((req, res, next) => {
     console.log('In the middleware !');
-    next();
+    next();     // Alows the request to continue to the next middleware in line
 });
 
 app.use((req, res, next) => {
     console.log('In the another middleware !');
+    res.send('<h1>Hello from Express !</h1>')
 });
 
 const server = http.createServer(app);
