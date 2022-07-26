983754b0dde5e9f0cad5f1a28b94ed2226ce1f21
NTD-Funix <ntd.funix@gmail.com>
2022-07-27 01:05:17 +0700

    LAB03.1_Sharing Data Across Request And Users

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin'); 
+const adminData = require('./routes/admin'); 
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
-const rootDir = require('../util/path');
-
 router.get('/', (req, res, next) => {
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
