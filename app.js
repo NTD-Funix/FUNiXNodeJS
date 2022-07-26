103718b529e05720d29460a1e3ffbbf49349f334
NTD-Funix <ntd.funix@gmail.com>
2022-07-26 17:29:53 +0700

    LAB02.13_Using a Helper Function for Navigation

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 const adminRoutes = require('./routes/admin'); 
 const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
@@ -13,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
