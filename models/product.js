3a4a1b7636aa68372acf187b33fd82b948cb7831
NTD-Funix <ntd.funix@gmail.com>
2022-08-02 00:09:08 +0700

    LAB04.4_Storing Data in Files Via the Model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require('fs');
+const p = require('path');
+
+const rootDir = require('../util/path');
 
 module.exports = class Product {
     constructor(t, i, p ,d) {
@@ -9,10 +12,26 @@ module.exports = class Product {
     };
 
     save() {
-        products.push(this);
+        const path = p.join(rootDir, 'data', 'products.json');
+        fs.readFile(path, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(path, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     };
 
     static fetchAll() {
-        return products;
+        const path = p.join(rootDir, 'data', 'products.json');
+        fs.readFile(path, (err, fileContent) => {
+            if(err) {
+                return [];
+            }
+            return JSON.parse(fileContent);
+        });
     };
 };
