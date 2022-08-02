7c6457951db524d2a49ed7630cf4105c0bfb63c9
NTD-Funix <ntd.funix@gmail.com>
2022-08-02 18:40:52 +0700

    LAB04.11_Passing Data with POST Requests

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,6 +38,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodID = req.body.productID;
+  console.log(prodID);
+  res.redirect('/cart');
+};
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
