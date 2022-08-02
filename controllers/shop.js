882e312a106af1e89d2f73d720d90d863b379133
NTD-Funix <ntd.funix@gmail.com>
2022-08-02 23:45:29 +0700

    LAB04.12_Adding a Cart Model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -40,7 +41,10 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodID = req.body.productID;
-  console.log(prodID);
+  Product.findById(prodID, product => {
+    console.log(typeof(product.price))
+    Cart.addProduct(prodID, product.price);
+  });
   res.redirect('/cart');
 };
 
