d4b5a65d26150187ebe8f681fa908c6f07c16c74
NTD-Funix <ntd.funix@gmail.com>
2022-08-04 17:37:54 +0700

    LAB04.21_Fixing a Delete Product Bug

--- models/cart.js
+++ models/cart.js
@@ -43,6 +43,9 @@ module.exports = class Cart {
             const card = JSON.parse(fileContent);
             const updatedCart = {...card};
             const product = updatedCart.products.find(product => product.id === id);
+            if (!product) {
+                return;
+            }
             const productQty = product.qty;
             updatedCart.products = updatedCart.products.filter(product => product.id !== id);
             updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
