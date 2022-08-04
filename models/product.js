1337b838d7813ae6eada7d4bf4b6e6b5721cbbb1
NTD-Funix <ntd.funix@gmail.com>
2022-08-05 00:57:21 +0700

    LAB05.6_Inserting Data Into the Database

--- models/product.js
+++ models/product.js
@@ -13,7 +13,8 @@ module.exports = class Product {
   }
 
   save() {
-    
+    return db.execute('INSERT INTO products(title, imageUrl, price, description) VALUES (?, ?, ?, ?)',
+            [ this.title, this.imageUrl, this.price, this.description ]);
   };
 
   static deleteById (id) {
