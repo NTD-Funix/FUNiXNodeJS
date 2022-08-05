4cede7fe7ed43cfe2f37d592a26f12b975580d53
NTD-Funix <ntd.funix@gmail.com>
2022-08-05 15:42:37 +0700

    LAB05.12_Retrieving Data & Finding Products using Sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,15 +2,14 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fielData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render('shop/product-list', {
-        prods: rows,
+        prods: products,
         pageTitle: 'All Products',
         path: '/products'
       });
-    })
-    .catch(err => console.log(err));
+    });
 };
 
 exports.getProduct = (req, res, next) => {
@@ -27,10 +26,10 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, datafield]) => {
+  Product.findAll()
+    .then((products) => {
       res.render('shop/index', {
-        prods: rows,
+        prods: products,
         pageTitle: 'Shop',
         path: '/'
       });
