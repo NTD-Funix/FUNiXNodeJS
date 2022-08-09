459bb85162d2625e228c3e0b47f15b19a412cb0e
NTD-Funix <ntd.funix@gmail.com>
2022-08-09 22:55:41 +0700

    LAB06.16_Working on Cart Items & Orders

--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const getDb = require('../util/database').getDb;
 
 const ObjectId = mongodb.ObjectId;
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.name = username;
         this.email = email;
+        this.cart = cart;
+        this._id = id;
     }
 
     save() {
@@ -20,6 +22,17 @@ class User {
             .catch(err => {
                 console.log(err);
             })
+    };
+
+    addToCart(product) {
+        // const cartProduct = this.cart.items.findIndex(cp => {
+        //     return cp._id === product._id;
+        // })
+        const updateCart = { items: [{ ...product, quantity: 1 }] };
+        const db = getDb();
+        return db
+            .collection('users')
+            .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updateCart } });
     }
 
     static findById(userId) {
