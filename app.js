c5445fa1b65f6a854d52a003e26c5f0897a84abc
NTD-Funix <ntd.funix@gmail.com>
2022-08-30 18:41:32 +0700

    LAB08.7_Using MongoDB to Store Sessions

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require("./controllers/error");
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://DaiNguyen:Maithuhuyen5893@cluster0.wzqosae.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+})
 
 // Template Engine
 app.set("view engine", "ejs");
@@ -25,7 +32,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(session({
   secret: 'My secret',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: store,
 }))
 
 app.use((req, res, next) => {
@@ -44,7 +52,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://DaiNguyen:Maithuhuyen5893@cluster0.wzqosae.mongodb.net/shop?retryWrites=true&w=majority')
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
