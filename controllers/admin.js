a963e05d1216c6a7c83128024ca5f42e04c88924
NTD-Funix <ntd.funix@gmail.com>
2022-09-23 15:24:43 +0700

    LAB10.22_Re-Using the Pagination Logic & Controls

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const Product = require("../models/product");
 const fileHelper = require('../util/file');
 
+const ITEMS_PER_PAGE = 3;
+
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
@@ -93,7 +95,18 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
+
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find({ userId: req.user._id })
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find({ userId: req.user._id })
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
     // .select('title price -_id')
     // .populate('userId', 'name')
     .then((products) => {
@@ -102,6 +115,12 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
